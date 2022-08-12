ae0754094c9a4a0fd27ee4542736a9defc38f412
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-12 17:10:09 +0300

    Create SimpleMath.cs

---
+++ CalculatorWPF/SimpleMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CalculatorWPF
+{
+    public static class SimpleMath
+    {
+        public static double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double Substruct(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public static double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double Divide(double number1, double number2)
+        {
+            if (number2 == 0)
+            {
+                // MessageBoxImage.Error ifadesini kullanarak mesaj kutusunda bir uyarı resmi ve bir uyarı sesi tanımlıyoruz.
+                MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+
+            return number1 / number2;
+        }
+    }
+}
